ec0e6c908923f1f1675656d97c4eed1386136833
Leandro Rodrigues <leandrocastro@trt15.jus.br>
2016-06-14 11:05:22 -0300

    atualizacoes na base para estoque

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 20160224183028) do
     t.string "title", limit: 100, null: false
   end
 
+  create_table "device_model_params", force: :cascade do |t|
+    t.string  "title",           limit: 100, null: false
+    t.integer "device_model_id", limit: 4,   null: false
+    t.integer "param_id",        limit: 4,   null: false
+  end
+
+  add_index "device_model_params", ["device_model_id"], name: "fk_device_model_params_device_models1_idx", using: :btree
+  add_index "device_model_params", ["param_id"], name: "fk_device_model_params_params1_idx", using: :btree
+
   create_table "device_models", force: :cascade do |t|
     t.string  "title",               limit: 100, null: false
     t.integer "device_brand_id",     limit: 4,   null: false
@@ -60,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160224183028) do
     t.integer "rack_units",          limit: 4
     t.integer "ethernet_interfaces", limit: 4
     t.integer "fiber_interfaces",    limit: 4
+    t.string  "stock_unit",          limit: 100
   end
 
   add_index "device_models", ["device_brand_id"], name: "fk_device_models_device_brands1_idx", using: :btree
@@ -115,9 +125,37 @@ ActiveRecord::Schema.define(version: 20160224183028) do
 
   add_index "items", ["number"], name: "number_UNIQUE", unique: true, using: :btree
 
+  create_table "params", force: :cascade do |t|
+    t.string "title", limit: 100, null: false
+    t.string "unit",  limit: 100, null: false
+  end
+
   create_table "places", force: :cascade do |t|
     t.string "title", limit: 100, null: false
   end
+
+  create_table "stock_items", force: :cascade do |t|
+    t.string  "title",            limit: 100,                 null: false
+    t.integer "item_id",          limit: 4
+    t.integer "device_model_id",  limit: 4,                   null: false
+    t.integer "container_id",     limit: 4,                   null: false
+    t.integer "quantity",         limit: 4
+    t.boolean "is_package",                   default: false, null: false
+    t.integer "package_quantity", limit: 4
+  end
+
+  add_index "stock_items", ["container_id"], name: "fk_stock_item_containers1_idx", using: :btree
+  add_index "stock_items", ["device_model_id"], name: "fk_stock_item_device_models1_idx", using: :btree
+  add_index "stock_items", ["item_id"], name: "fk_stock_item_items1_idx", using: :btree
+
+  create_table "stock_param_values", force: :cascade do |t|
+    t.text    "value",                 limit: 65535, null: false
+    t.integer "stock_item_id",         limit: 4,     null: false
+    t.integer "device_model_param_id", limit: 4,     null: false
+  end
+
+  add_index "stock_param_values", ["device_model_param_id"], name: "fk_stock_param_value_device_model_param1_idx", using: :btree
+  add_index "stock_param_values", ["stock_item_id"], name: "fk_stock_param_value_stock_item1_idx", using: :btree
 
   add_foreign_key "connections", "interfaces", column: "interface0_id", name: "fk_connections_interfaces1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "connections", "interfaces", column: "interface1_id", name: "fk_connections_interfaces2", on_update: :cascade, on_delete: :cascade
@@ -126,10 +164,17 @@ ActiveRecord::Schema.define(version: 20160224183028) do
   add_foreign_key "containers", "container_types", name: "fk_containers_container_types1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "containers", "items", name: "fk_containers_items1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "containers", "places", name: "fk_containers_places", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "device_model_params", "device_models", name: "fk_device_model_params_device_models1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "device_model_params", "params", name: "fk_device_model_params_params1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "device_models", "device_brands", name: "fk_device_models_device_brands1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "device_models", "device_types", name: "fk_device_models_device_types1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "devices", "device_models", name: "fk_devices_device_models1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "devices", "items", name: "fk_devices_items1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "interfaces", "devices", name: "fk_interfaces_devices1", on_update: :cascade, on_delete: :cascade
   add_foreign_key "interfaces", "interface_types", name: "fk_interfaces_interface_types1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "stock_items", "containers", name: "fk_stock_item_containers1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "stock_items", "device_models", name: "fk_stock_item_device_models1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "stock_items", "items", name: "fk_stock_item_items1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "stock_param_values", "device_model_params", name: "fk_stock_param_value_device_model_param1", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "stock_param_values", "stock_items", name: "fk_stock_param_value_stock_item1", on_update: :cascade, on_delete: :cascade
 end
